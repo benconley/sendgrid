5a80b32fdb27623f9d0ee1a959fa4d325b5846b1
bcphpuser <bcphpuser@cactus.dreamhost.com>
2017-03-07 19:31:28 -0800

    Swap JSON implemenation via curl in place of Namespaced PHP objects

--- service/sendmail.php
+++ service/sendmail.php
@@ -8,7 +8,7 @@ class sendemail{
 	protected $toemail, $fromemail, $subject, $message, $apiKey;
 	protected $delaySecs = 300;
 	public function __construct() {
-		$this->apiKey = file_get_contents('/home/bcphpuser/backups/sendgrid.env');
+		$this->apiKey = trim(file_get_contents('/home/bcphpuser/backups/sendgrid.env'));
 		$this->validate();
 		if ($this->respObj['status']) {
 			$this->submitMail();
@@ -54,57 +54,70 @@ class sendemail{
 	}
 
 	protected function submitMail() {
-		$mail = new SendGrid\Mail();
-		$email = new SendGrid\Email($this->fromemail, $this->fromemail);
+		// build submission object
+		$mailObj = [
+			"personalizations" => [[
+				"to" => [["email" => $this->toemail, "name" => $this->toemail]],
+				"subject" => $this->subject,
+				"headers" => ["X-Accept-Language" => "en", "X-Mailer" => "MyApp"],
+				"custom_args" => [
+					"Developer" => "Ben",
+					"Company" => "FilterEasy",
+					"Email_Name" => "test"
+				],
+				"send_at" => time() + intval($this->delaySecs)
+			]],
+			"from" => ["email" => $this->fromemail, "name" => $this->fromemail],
+			"content" => [["type" => "text/html", "value" => $this->message]]
+		];
 
-		// from
-		$mail->setFrom($email);
-
-		//personalization object
-		$personalization = new SendGrid\Personalization();
-
-		// destination
-		$email = new SendGrid\Email($this->toemail, $this->toemail);
-		$personalization->addTo($email);
-
-		//subject
-		$personalization->setSubject($this->subject);
-
-		// set delay
-		$personalization->setSendAt(time() + intval($this->delaySecs));
-
-		// custom args
-		$personalization->addCustomArg("Developer", "Ben");
-		$personalization->addCustomArg("Company", "FilterEasy");
-		$personalization->addCustomArg("Email_Name", "test");
-		$mail->addPersonalization($personalization);
-
-		// content
-		$content = new SendGrid\Content("text/plain", $this->message);
-		$mail->addContent($content);
-
-		// attachment
-		if(is_uploaded_file($_FILES['attachment']['tmp_name'])){
-			$attachment = new SendGrid\Attachment();
-			$attachment->setContent(base64_encode(file_get_contents($_FILES['attachment']['tmp_name'])));
-			$attachment->setType($_FILES['attachment']['type']);
-			$attachment->setFilename($_FILES['attachment']['name']);
-			$attachment->setDisposition("attachment");
-			$attachment->setContentId("Attachment");
-			$mail->addAttachment($attachment);
+		if(is_uploaded_file($_FILES['attachment']['tmp_name'])) {
+			$mailObj["attachments"] = array(
+				array(
+					"content" => base64_encode(file_get_contents($_FILES['attachment']['tmp_name'])),
+					"type" => $_FILES['attachment']['type'],
+					"name" => $_FILES['attachment']['name'],
+					"filename" => $_FILES['attachment']['name'],
+					"content_id" => uniqid()
+				)
+			);
 		}
+		$curl = curl_init();
 
-		$sg = new \SendGrid($this->apiKey);
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode($mailObj),
+			CURLOPT_HTTPHEADER => array(
+			"authorization: Bearer $this->apiKey",
+			"content-type: application/json"
+			),
+		));
 
-		// send mail
-		$response = $sg->client->mail()->send()->post($mail);
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
 
-		// handle any error response
-		if (intval($response->statusCode()) >= 400 ) {
+		curl_close($curl);
+		if ($err) {
 			$this->respObj['status'] = false;
-			$this->respObj['message'] = 'An error was returned from the mailer service';
-			$this->respObj['detail'] = $response->body();
+			$this->respObj['message'] = 'cURL Error #:' . $err;
+		} else {
+			if(strlen($response)) {
+				$errArr = json_decode($response);
+				if ($errArr) {
+					$errPtr = $errArr->errors[0];
+					$errPtr->message;
+					$this->respObj['status'] = false;
+					$this->respObj['message'] = $errPtr->message; 
+				}
+			}
 		}
+
 	}
 
 	protected function yieldResponse() {
